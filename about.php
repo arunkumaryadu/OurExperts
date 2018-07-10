a0158996f3d4f87a7286cac828966ecac3a59214
Arun Kumar <30272820+arun94255@users.noreply.github.com>
2018-07-10 23:18:46 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,20 @@
+<?php $page="about" ?>
+<?php require_once 'header.php'; ?>
+<img src="images/abtus.png" style="width: 100%; height: 200px" />
+<p>The primary objective of the OurExperts.com is to provide instant access to the latest information and resources on every facet of education.</p>
+<p>OurExperts.com designed with a vision to cover the entire college with dedicated portals for everyone.</p>
+<h2>Why Career OurExperts.com?</h2>
+<p>HELPS IN IDENTIFING A STUDENT'S TRUE POTENTIAL - Identifying their strengths & weaknesses and also the area of interest This is the place where they can build a career for life.</p>
+<p>CLEARS CONFUSION - Compare and contrast the required education and career potential with your personal goals.
+HELPS TO MAKE AN INFORMED DECISION - Counselling helps the students to make clear and informed decisions about theirfuture.</p>
+<h2>How do we help?</h2>
+<p>We bridge the gap between students and their career path.
+We impart right and updated information about courses & careers.></p>
+<ul>
+<li>Which stream to choose after engg?</li>
+<li>How to prepare for exams?</li>
+<li>Which postgraduate degree to pursue after engg?</li>
+<li>How to manage exam stress?</li>
+<li>How to manage time and study effectively?</li>
+</ul>
+<?php require_once 'footer.php'; ?>
